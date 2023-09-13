f511c65a7421422f376be98d6386c7079039dfb3
Emir KORKUT <emirkorkut.d@gmail.com>
2023-09-13 21:39:27 +0300

    added navbar and some changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
     import "../app.css"
 </script>
-<!-- 
-<nav class="  border md:border-0 fixed z-50 px-6 py-4 md:px-16 md:py-8 flex justify-center space-x-6 md:space-x-16 backdrop-blur-sm bg-white bg-opacity-50 top-6 right-1/2 translate-x-1/2 h-auto text-xl font-semibold rounded-full text-red-700 ">
+
+<nav class="  border border-r-0 fixed z-50 px-6 py-6 md:px-16 md:py-8 flex justify-center space-x-6 md:space-x-16 backdrop-blur-sm bg-white bg-opacity-50 top-6 right-0 translate-x-0  h-auto text-xl md:text-2xl font-semibold rounded-l-full  text-red-700 ">
     <a href="/">
         Home    
     </a>
@@ -15,11 +15,37 @@
     <a href="/">
         Prices
     </a>
-</nav> -->
+</nav>
 
     <slot/>
 <style lang="postcss">
     :global(html){
         @apply scroll-smooth;
+
+
     }
+
+    /* width */
+    :global(::-webkit-scrollbar) {
+  width: 10px;
+}
+
+/* Track */
+:global(::-webkit-scrollbar-track) {
+  @apply bg-red-700 transition-all duration-500;
+}
+/* Track */
+:global(::-webkit-scrollbar-track:hover) {
+  @apply bg-gray-200 transition-all duration-500;
+}
+
+/* Handle */
+:global(::-webkit-scrollbar-thumb) {
+    @apply bg-white transition-all duration-500;
+}
+
+/* Handle on hover */
+:global(::-webkit-scrollbar-thumb:hover) {
+    @apply bg-red-700 transition-all duration-500;
+}
 </style>
