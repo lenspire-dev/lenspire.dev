6de2679fe62753d3995c0042d7c04eef26662460
Lenspire.dev <emirkorkut.d@gmail.com>
2024-01-29 21:29:29 +0000

    Coming Soon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,48 +5,5 @@
     <title>Lenspire.dev | Full-Stack Developer </title>
     <meta name="description" content="I am Lenspire and i am a full-stack developer. Contact me via WhatsApp.">
 </svelte:head>
-<nav class="  border border-r-0 fixed z-50 px-10 py-6 md:px-16 md:py-8 flex justify-center space-x-10 md:space-x-16 backdrop-blur-sm bg-white bg-opacity-50 top-6 right-0 translate-x-0  h-auto text-xl md:text-2xl font-semibold rounded-l-full  text-red-700 ">
-    <a href="/">
-        Home    
-    </a>
-    <a href="/#whoami">
-        About
-    </a>
-    <a href="/#contact">
-        Contact
-    </a >
-    
-</nav>
 
     <slot/>
-<style lang="postcss">
-    :global(html){
-        @apply scroll-smooth;
-
-
-    }
-
-    /* width */
-    :global(::-webkit-scrollbar) {
-  width: 10px;
-}
-
-/* Track */
-:global(::-webkit-scrollbar-track) {
-  @apply bg-red-700 transition-all duration-500;
-}
-/* Track */
-:global(::-webkit-scrollbar-track:hover) {
-  @apply bg-gray-200 transition-all duration-500;
-}
-
-/* Handle */
-:global(::-webkit-scrollbar-thumb) {
-    @apply bg-white transition-all duration-500;
-}
-
-/* Handle on hover */
-:global(::-webkit-scrollbar-thumb:hover) {
-    @apply bg-red-700 transition-all duration-500;
-}
-</style>
