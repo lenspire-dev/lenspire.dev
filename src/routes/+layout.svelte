d54b88ef3b60913be4f7e2770e8c09c2bc716815
Lenspire.dev <emirkorkut.d@gmail.com>
2024-01-29 21:36:05 +0000

    Coming Soon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,8 @@
 </script>
 <svelte:head>
     <title>Lenspire.dev | Full-Stack Developer </title>
-    <meta name="description" content="I am Lenspire and i am a full-stack developer. Contact me via WhatsApp.">
+    <meta name="description" content="Lenspire.dev'e hoş geldin!
+    Bu sitede yazılım eğitimleri, projeler, etkinlikler, canlı yayınlar gerçekleştirmenin yanı sıra seninle beraber Türkiye'nin en büyük yazılım topluluğunu oluşturup sektöre beraber yön vermeyi amaçlıyorum. Bir elin nesi var iki elin sesi var, hadi başlayalım :]">
 </svelte:head>
 
     <slot/>
